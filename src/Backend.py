5d0b53cd3a7572c5c541230b8a5d4616bb8041f4
Wesley <wesleypacs@gmail.com>
2021-05-12 11:02:03 -0700

    Tracker init Tracker Tests for update

--- src/Backend.py
+++ src/Backend.py
@@ -16,3 +16,26 @@ class Car:
         self.laptimes.append(difference)
         self.lapsCompleted += 1
         self.time = newtime
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.number == other.number
+        else:
+            return False
+
+
+class Tracker:
+    def __init__(self, cars=()):
+        self.createCars(cars)
+
+    def createCars(self, carNums):
+        self.cars = {}
+        self.positions = []
+        for carNum in carNums:
+            car = Car(carNum)
+            self.cars[carNum] = car
+            self.positions.append(car)
+
+    def update(self, car, time):
+        self.cars[car].update(time)
+        # sort the list since that is the postions
--- test/test_Backend.py
+++ test/test_Backend.py
@@ -10,15 +10,34 @@ def test_Car():
     assert testCar.time == timedelta()
 
     # Update
+    # First lap
     time1 = timedelta(minutes=1, seconds=20)
     testCar.update(time1)
     assert testCar.lapsCompleted == 1
     assert testCar.time == time1
     assert testCar.laptimes[0] == time1
 
+    # Second lap
     time2 = timedelta(minutes=2, seconds=30)
     difference = time2-time1
     testCar.update(time2)
     assert testCar.lapsCompleted == 2
     assert testCar.time == time2
     assert testCar.laptimes[1] == difference
+
+
+def test_Tracker():
+    # init
+    testTracker = Tracker((2, 4, 9))
+    referenceDict = {2: Car(2), 4: Car(4), 9: Car(9)}
+    referenceList = [Car(2), Car(4), Car(9)]
+    assert testTracker.cars == referenceDict
+    assert testTracker.positions == referenceList
+
+    # update
+    timeCar9 = timedelta(minutes=1, seconds=20)
+    testTracker.update(9, timeCar9)
+    timeCar2 = timedelta(minutes=2, seconds=30)
+    testTracker.update(2, timeCar2)
+    referenceList = [Car(9), Car(2), Car(4)]
+    assert testTracker.positions == referenceList
